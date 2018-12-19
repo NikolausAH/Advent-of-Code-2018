baafee760d91c619abfa4d366def28fa37e707e7
NikolausAH <Nikolausaldoh@gmail.com>
2018-12-19 21:23:05 +0700

    Day 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args){
         Integer totalFrequency = 0;
-        Day1 day1 = new Day1();
-        System.out.println(day1.day1(totalFrequency));
+        Day2 day2 = new Day2();
+        day2.day2();
     }
 }
