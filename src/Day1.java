904ebd3f170e66dd12a9629a404e3d2dbb275bcc
NikolausAH <Nikolausaldoh@gmail.com>
2018-12-19 18:42:33 +0700

    Day1 Completed

--- src/Day1.java
+++ src/Day1.java
@@ -1,14 +1,26 @@
 import javax.sound.sampled.Line;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Day1 {
-    public Integer day1(Integer totalFrequency){
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    public Integer day1(Integer totalFrequency) {
         Scanner scanner = new Scanner(System.in);
-        while(scanner.hasNextInt()) {
-        totalFrequency+=scanner.nextInt();
-        if(scanner.nextLine()==null){
-            break;
-        }
+        while (scanner.hasNextInt()) {
+            map.put(totalFrequency,totalFrequency);
+            totalFrequency += scanner.nextInt();
+            if(map.containsKey(totalFrequency)){
+                System.out.println("BREAK");
+                System.out.println(totalFrequency);
+                break;
+            }
+            else{
+                map.put(totalFrequency,totalFrequency);
+            }
+            if (scanner.nextLine() == null) {
+                break;
+            }
         }
         return totalFrequency;
     }
